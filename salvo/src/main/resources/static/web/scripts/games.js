fa2d9de194f59ec362b0f72e854a87b24e5c51fd
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-09-20 19:40:59 -0300

    Emprolijado posterior al submit

--- salvo/src/main/resources/static/web/scripts/games.js
+++ salvo/src/main/resources/static/web/scripts/games.js
@@ -1,21 +1,28 @@
 $(function(){
 
-  function showGameList(gameList){
-    var li="";
-    gameList.forEach(g => {
-      var date = new Date(g.created).toLocaleString();
-      var players = g.gamePlayers.map(gp => gp.player.email).join(', ');
-      li+='<li>'+ date + ', ' + players + '</li>';
-    });
-    $('#gameList').html(li);
+  function getTotal(name){
+    return scoreList.filter(s => s.name == name).reduce((total, s) => total + s.score, 0);
+  }
+
+  function getWons(name){
+    return scoreList.filter(s => s.name == name && s.score == 1).length;
+  }
+
+  function getLosts(name){
+    return scoreList.filter(s => s.name == name && s.score == 0).length;
+  }
+
+  function getTieds(name){
+      return scoreList.filter(s => s.name == name && s.score == 0.5).length;
   }
 
   // Devuelve un JSON con la tabla de jugadores con puntajes mas altos
   function getLeaderTable(gameList){
+    scoreList=[]; // Lista de nombres y puntajes de todos los juegos
+    var nameList=[]; // Lista de nombres de jugadores (no repetidos)
+    var leaderTable=[]; // JSON de la LeaderTable
 
-    scoreList=[]; // Lista de names y scores
-    var nameList=[];
-    var leaderTable=[];
+    // Obtiene la lista de jugadores con puntajes
     gameList.forEach(game => {
       game.gamePlayers.forEach(gp => {
         scoreList.push(
@@ -26,43 +33,42 @@ $(function(){
         );
       });
     });
+    // Elimina de la lista los puntajes de juegos no terminados
     scoreList = scoreList.filter(player => player.score != null);
 
-    // Lista de nombres unicos
+    // Obtiene la lista de nombres unicos (no repetidos)
     nameList = scoreList.map(s => s.name).
       filter((n,i,nl) => nl.indexOf(n)==i);
 
+    // Para cada jugador de la lista, agrega una linea de datos al JSON
     nameList.forEach(name => {
       leaderTable.push(
         {
           name: name,
           total: getTotal(name).toFixed(1),
-          won: getWon(name),
-          lost: getLost(name),
-          tied: getTied(name)
+          won: getWons(name),
+          lost: getLosts(name),
+          tied: getTieds(name)
         }
       );
     });
 
+    // Ordena las lineas del JSON por puntajes decrecientes
     return leaderTable.sort((l1, l2) => l2.total - l1.total);
   }
 
-  function getTotal(name){
-    return scoreList.filter(s => s.name == name).reduce((total, s) => total + s.score, 0);
+  // Construye una lista ordenada de Games con su informacion y la devuelve al HTML
+  function showGameList(gameList){
+    var li="";
+    gameList.forEach(g => {
+      var date = new Date(g.created).toLocaleString();
+      var players = g.gamePlayers.map(gp => gp.player.email).join(', ');
+      li+='<li>'+ date + ', ' + players + '</li>';
+    });
+    $('#gameList').html(li);
   }
 
-  function getWon(name){
-    return scoreList.filter(s => s.name == name && s.score == 1).length;
-  }
-
-  function getLost(name){
-    return scoreList.filter(s => s.name == name && s.score == 0).length;
-  }
-
-  function getTied(name){
-      return scoreList.filter(s => s.name == name && s.score == 0.5).length;
-  }
-
+  // Construye una tabla de estadisticas de puntajes de jugadores y la devuelve al HTML
   function showLeaderBoard(gameList){
     var table = "<tr>\
                   <th>Name</th><th>Total</th><th>Won</th><th>Lost</th><th>Tied</th>\
